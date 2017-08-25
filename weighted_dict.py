4388b49f091bb55e3bed9f9429b584500651cc91
phil-mcdowall <philipsmcdowall@gmail.com>
2017-08-24 20:43:51 -0400

    add init from dicts

--- weighted_dict.py
+++ weighted_dict.py
@@ -1,10 +1,17 @@
 import randomdict
 
 class WeightedDict:
-    def __init__(self):
+    def __init__(self, **kwargs):
         self._data = dict()
         self._weights = dict()
         self._total = 0
+        if all(k in kwargs for k in ['weights','data']):
+            self._data = kwargs['data']
+            self._weights = kwargs['weights']
+            self._total = self._calculate_total()
+
+    def _calculate_total(self):
+        return sum(self._weights.values())
 
     def __getitem__(self, key):
         return self._data.__getitem__(key)
@@ -15,7 +22,6 @@ class WeightedDict:
         if value[1] < 0:
             raise ValueError("Weights must be positive")
         if key in self._data:
-            print("modifying")
             self._total -= self._weights[key]
             self._total += value[1]
             self._weights[key] = value[1]
@@ -38,10 +44,10 @@ class WeightedDict:
         return self._data.__contains__(key)
 
     def copy(self):
-        return type(self)(self)
+        return type(self)(**{'data':self._data,'weights':self._weights})
 
     def __repr__(self):
-        return '{0}(data:{1},weights:{2})'.format(type(self).__name__, self._data.__repr__(), self._weights.__repr__())
+        return '{0}({{data:{1},weights:{2}}})'.format(type(self).__name__,self._data.__repr__(), self._weights.__repr__())
 
     def random(self, k):
         return [self._data[i] for i in randomdict.random(k, self._weights, self._total)]
